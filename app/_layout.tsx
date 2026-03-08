57d75310abcce832607fbc39f185f63e7d8e0f9e
Kucze3205 <kuczerowski2005@gmail.com>
2026-03-08 21:01:05 +0100

    feat: reorder import statements for consistency in layout and auth modal components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
